dcbff057f5100433ba2e71e1a05159ed8871d77c
Shawn <lcorel86@gmail.com>
2026-05-14 13:53:50 -0500

    testing in main

--- OOPMasterChallenge/src/Main.java
+++ OOPMasterChallenge/src/Main.java
@@ -2,5 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        MealOrder defaultOrder = new MealOrder();
+        System.out.println(defaultOrder);
     }
 }
--- OOPMasterChallenge/src/MealOrder.java
+++ OOPMasterChallenge/src/MealOrder.java
@@ -3,20 +3,50 @@ public class MealOrder {
     private Burger burger;
     private Drink drink;
     private SideItem side;
+    private boolean isDeluxe = false;
+    private double orderTotal = 0;
 
     public MealOrder() {
         this.burger = new Burger("Common", 5);
         this.drink = new Drink("coke", "small", 1);
         this.side = new SideItem("fries", 1.5);
+        this.orderTotal += (burger.getPrice() + drink.getPrice() + side.getPrice());
     }
 
+    public MealOrder(Burger burger, Drink drink, SideItem side) {
+        this.burger = burger;
+        this.drink = drink;
+        this.side = side;
+        this.orderTotal += (burger.getPrice() + drink.getPrice() + side.getPrice());
+    }
+
+    public MealOrder(DeluxeBurger deluxe, Drink drink, SideItem side) {
+        this.isDeluxe = true;
+        this.burger = deluxe;
+        this.drink = drink;
+        this.side = side;
+        this.orderTotal += (burger.getPrice() + drink.getPrice() + side.getPrice());
+    }
+
+    public void addToppings(String topping, double price) {
+        this.burger.addTopping(topping, price);
+    }
+
+    @Override
+    public String toString() {
+        return "MealOrder details --" +
+                " burger -" + burger.toString() +
+                ", drink -" + drink.toString() +
+                ", side -" + side.toString() +
+                ", orderTotal = " + orderTotal;
+    }
 }
 
 class Toppings {
 
-    private String topping1;
-    private String topping2;
-    private String topping3;
+    protected String topping1;
+    protected String topping2;
+    protected String topping3;
 
     public Toppings() {
         this.topping1 = "";
@@ -39,13 +69,18 @@ class Toppings {
         }
     }
 
+
     @Override
     public String toString() {
-        return "Toppings{" +
-                "topping1='" + topping1 + '\'' +
-                ", topping2='" + topping2 + '\'' +
-                ", topping3='" + topping3 + '\'' +
-                '}';
+        String baseStr = " ";
+        if (!this.topping1.isEmpty()) {
+            baseStr.concat(this.topping1 + " ");
+        } else if (!this.topping1.isEmpty()) {
+            baseStr.concat(this.topping2 + " ");
+        } else if (!this.topping3.isEmpty()) {
+            baseStr.concat(this.topping3 + " ");
+        }
+        return "No extra toppings";
     }
 }
 
@@ -79,17 +114,26 @@ class DeluxToppings extends Toppings {
 
     @Override
     public String toString() {
-        return "DeluxToppings{" +
-                "extraTopping1='" + extraTopping1 + '\'' +
-                ", extraTopping2='" + extraTopping2 + '\'' +
-                "} " + super.toString();
+        String baseStr = " ";
+        if (!this.topping1.isEmpty()) {
+            baseStr.concat(this.topping1 + " ");
+        } else if (!this.topping1.isEmpty()) {
+            baseStr.concat(this.topping2 + " ");
+        } else if (!this.topping3.isEmpty()) {
+            baseStr.concat(this.topping3 + " ");
+        } else if (!this.extraTopping1.isEmpty()) {
+            baseStr.concat(this.extraTopping1 + " ");
+        } else if (!this.extraTopping2.isEmpty()) {
+            baseStr.concat(this.extraTopping2 + " ");
+        }
+        return "No extra toppings";
     }
 }
 
 class Burger {
 
-    private String type;
-    private Toppings toppings;
+    protected String type;
+    protected Toppings toppings;
     protected double price;
 
     public Burger(String type, double price) {
@@ -105,6 +149,15 @@ class Burger {
         return 0;
     }
 
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        String baseStr = " " + this.type + " with ";
+        return " " + this.type + " with " + this.toppings.toString();
+    }
 }
 
 class DeluxeBurger extends Burger {
@@ -119,7 +172,6 @@ class DeluxeBurger extends Burger {
     @Override
     public int addTopping(String topping, double price) {
         int result = toppings.addToppings(topping);
-        super.price += price;
         if (result == -1) {
             System.out.println("You can't add anymore toppings");
             return result;
@@ -127,6 +179,11 @@ class DeluxeBurger extends Burger {
         return 0;
     }
 
+    @Override
+    public String toString() {
+        String baseStr = "Burger: " + this.type + " with ";
+        return " " + this.type + " with " + this.toppings.toString();
+    }
 }
 
 class Drink {
@@ -140,6 +197,16 @@ class Drink {
         this.size = size;
         this.price = price;
     }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return " " + this.size + " " + this.type;
+    }
+
 }
 
 class SideItem {
@@ -150,5 +217,14 @@ class SideItem {
     public SideItem(String type, double price) {
         this.type = type;
         this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return " " + this.type;
     }
 }
