8e2f12b95fff668142d88f562f1ed2667638ba90
Shawn <lcorel86@gmail.com>
2026-05-20 18:39:33 -0500

    on using arrays

--- ArrayProject/src/dev/lpa/Main.java
+++ ArrayProject/src/dev/lpa/Main.java
@@ -1,4 +1,5 @@
 package dev.lpa;
+import java.util.Arrays;
 
 public class Main {
 
@@ -20,9 +21,29 @@ public class Main {
         System.out.println("last = " + firstTen[arrayLength - 1]);
 
         int[] newArray;
-        newArray = new int[] {5, 4, 3, 2, 1};
-        for (int i = 0; i < newArray.length; i ++) {
+//        newArray = new int[] {5, 4, 3, 2, 1};
+        newArray = new int[5];
+        for (int i = 0; i < newArray.length; i++) {
+            newArray[i] = newArray.length - i;
+        }
+        for (int i = 0; i < newArray.length; i++) {
             System.out.println(newArray[i] + " ");
         }
+        System.out.println();
+        for (int element : newArray) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+        System.out.println(Arrays.toString(newArray));
+        Object objectVariable = newArray;
+        if (objectVariable instanceof int[]) {
+            System.out.println("objectVariable is really an int array");
+        }
+
+        Object[] objectArray = new Object[3];
+        objectArray[0] = "Hello";
+        objectArray[1] = new StringBuilder("World");
+        objectArray[2] = newArray;
+
     }
 }
