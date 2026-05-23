5553f9df03aae5cbad47653ba2b62ffaf12e68b0
Shawn <lcorel86@gmail.com>
2026-05-22 19:18:36 -0500

    sorting with loops

--- ArrayChallenge/src/Main.java
+++ ArrayChallenge/src/Main.java
@@ -11,13 +11,30 @@ public class Main {
             int num = random.nextInt(100);
             intArr[i] = num;
         }
-        int[] decIntArr = new int[5];
-        Arrays.sort(intArr);
-        System.out.println(Arrays.toString(intArr));
-        int len = intArr.length;
-        for (int i = len - 1; i >= 0; i--) {
-            decIntArr[len - 1 - i] = intArr[i];
+        int[] decIntArr = Arrays.copyOf(intArr, intArr.length);
+//        Arrays.sort(intArr);
+//        System.out.println(Arrays.toString(intArr));
+//        int len = intArr.length;
+//        for (int i = len - 1; i >= 0; i--) {
+//            decIntArr[len - 1 - i] = intArr[i];
+//        }
+        System.out.println(Arrays.toString(decIntArr));
+
+        boolean flag = true;
+        int temp;
+        while (flag) {
+            flag = false;
+            for (int i = 0; i < decIntArr.length - 1; i++) {
+                if (decIntArr[i] < decIntArr[i + 1]) {
+                    temp = decIntArr[i];
+                    decIntArr[i] = decIntArr[i + 1];
+                    decIntArr[i + 1] = temp;
+                    flag = true;
+                    System.out.println("------>" + Arrays.toString(decIntArr));
+                }
+            }
         }
+
         System.out.println(Arrays.toString(decIntArr));
     }
 }
