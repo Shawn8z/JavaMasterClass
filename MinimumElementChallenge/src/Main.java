46a3510fa1ca7a70161c9fa797e0e2a132cf7880
Shawn <lcorel86@gmail.com>
2026-05-23 23:16:36 -0500

    MinVal challenge

---
+++ MinimumElementChallenge/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        System.out.println(Arrays.toString(readIntegers()));
+        int[] numArr = readIntegers();
+        System.out.println(findMin(numArr));
+//        int[] test = {11, 2, 3, 5, 6, 7};
+//        System.out.println(findMin(test));
+    }
+
+    public static int[] readIntegers() {
+        System.out.printf("Enter a list of numbers%n");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] strArr = input.split(",");
+        int len = strArr.length;
+        int[] numArr = new int[len];
+        for (int i = 0; i < len; i++) {
+            numArr[i] = Integer.parseInt(strArr[i]);
+        }
+        return numArr;
+    }
+
+    public static int findMin(int[] array) {
+        int min = Integer.MAX_VALUE;
+        for (int num : array) {
+            if (num < min) {
+                min = num;
+            }
+        }
+        return min;
+    }
+}
