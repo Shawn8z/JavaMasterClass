47ad8942df8a8bd51250737e2a6e46e9290da994
Shawn <lcorel86@gmail.com>
2026-05-26 20:18:56 -0500

    array list

--- ReverseArray/src/Main.java
+++ ReverseArray/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         int[] testArr = {1, 2, 3, 4, 5};
-//        System.out.println(Arrays.toString(testArr));
+        System.out.println(Arrays.toString(testArr));
 //        int[] resultArr = reverse(testArr);
 //        System.out.println(Arrays.toString(resultArr));
         reverse(testArr);
@@ -22,24 +22,37 @@ public class Main {
 //        return resultArr;
 //    }
 
+//    private static void reverse(int[] array) {
+//
+//        int maxIndex = array.length - 1;
+//        int halfLength = array.length / 2;
+//
+//        for (int i = 0; i < halfLength; i++) {
+//            int temp = array[i];
+//            array[i] = array[maxIndex - i];
+//            array[maxIndex - i] = temp;
+//        }
+//    }
+
+//    private static int[] reverseCopy(int[] array) {
+//        int[] reversedArray = new int[array.length];
+//        int maxIndex = array.length - 1;
+//        for (int el : array) {
+//            reversedArray[maxIndex--] = el;
+//        }
+//        return reversedArray;
+//    }
+
     private static void reverse(int[] array) {
-
-        int maxIndex = array.length - 1;
-        int halfLength = array.length / 2;
-
-        for (int i = 0; i < halfLength; i++) {
+        System.out.println("Array = " + Arrays.toString(array));
+        int len = array.length - 1;
+        int halfLen = len / 2;
+        for (int i = 0; i <= halfLen; i++) {
             int temp = array[i];
-            array[i] = array[maxIndex - i];
-            array[maxIndex - i] = temp;
+            if (array[i] == array[len - i]) continue;
+            array[i] = array[len - i];
+            array[len - i] = temp;
         }
-    }
-
-    private static int[] reverseCopy(int[] array) {
-        int[] reversedArray = new int[array.length];
-        int maxIndex = array.length - 1;
-        for (int el : array) {
-            reversedArray[maxIndex--] = el;
-        }
-        return reversedArray;
+        System.out.println("Reversed array = " + Arrays.toString(array));
     }
 }
