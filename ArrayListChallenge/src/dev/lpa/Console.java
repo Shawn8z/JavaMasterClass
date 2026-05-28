dec90a33917d9598f0b04295dc07017cf22c5db9
Shawn <lcorel86@gmail.com>
2026-05-27 21:30:48 -0500

    video version done

--- ArrayListChallenge/src/dev/lpa/Console.java
+++ ArrayListChallenge/src/dev/lpa/Console.java
@@ -1,61 +1,64 @@
 package dev.lpa;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class Console {
 
-//    ArrayList<String> arrList = new ArrayList<>();
-//
-//    public void runProgram() {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Available actions:");
-//        System.out.println("0 - to shutdown");
-//        System.out.println("1 - to add item(s) to list (comma delimited list)");
-//        System.out.println("2 - to remove any items (comma delimited list)");
-//        System.out.println("Enter a number for which action you want to do");
-//        int input = scanner.nextInt();
-//        switch (input) {
-//            case 1 -> addItem();
-//            case 2 -> removeItem();
-//            default -> scanner.close();
-//        }
-//    }
-//
-//    public void addItem() {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("To add item(s) to list (comma delimited list)");
-//        String str = scanner.nextLine();
-//        boolean hasComma = str.indexOf(',') != -1;
-//        if (hasComma) {
-//            String[] strArr = str.split(",");
-//            for (String item : strArr) {
-//                if (arrList.contains(item.trim())) continue;
-//                arrList.add(item.trim());
-//            }
-//        } else {
-//            if (arrList.contains(str)) return;
-//            arrList.add(str);
-//        }
-//        arrList.sort(Comparator.naturalOrder());
-//        System.out.println(arrList);
-//        runProgram();
-//    }
-//
-//    public void removeItem() {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("To remove any items (comma delimited list)");
-//        String str = scanner.nextLine();
-//        boolean hasComma = str.indexOf(',') != -1;
-//        if (hasComma) {
-//            ArrayList<String> tempList = new ArrayList<>();
-//            String[] strArr = str.split(",");
-//            for (String item : strArr) {
-//                tempList.add(item.trim());
-//            }
-//            arrList.removeAll(tempList);
-//        } else {
-//            arrList.remove(str.trim());
-//        }
-//        arrList.sort(Comparator.naturalOrder());
-//        System.out.println(arrList);
-//        runProgram();
-//    }
+    ArrayList<String> arrList = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
+
+    public void runProgram() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Available actions:");
+        System.out.println("0 - to shutdown");
+        System.out.println("1 - to add item(s) to list (comma delimited list)");
+        System.out.println("2 - to remove any items (comma delimited list)");
+        System.out.println("Enter a number for which action you want to do");
+        int input = scanner.nextInt();
+        switch (input) {
+            case 1 -> addItem();
+            case 2 -> removeItem();
+            default -> scanner.close();
+        }
+    }
+
+    public void addItem() {
+        System.out.println("To add item(s) to list (comma delimited list)");
+        String str = scanner.nextLine();
+        boolean hasComma = str.indexOf(',') != -1;
+        if (hasComma) {
+            String[] strArr = str.split(",");
+            for (String item : strArr) {
+                if (arrList.contains(item.trim())) continue;
+                arrList.add(item.trim());
+            }
+        } else {
+            if (arrList.contains(str)) return;
+            arrList.add(str);
+        }
+        arrList.sort(Comparator.naturalOrder());
+        System.out.println(arrList);
+        runProgram();
+    }
+
+    public void removeItem() {
+        System.out.println("To remove any items (comma delimited list)");
+        String str = scanner.nextLine();
+        boolean hasComma = str.indexOf(',') != -1;
+        if (hasComma) {
+            ArrayList<String> tempList = new ArrayList<>();
+            String[] strArr = str.split(",");
+            for (String item : strArr) {
+                tempList.add(item.trim());
+            }
+            arrList.removeAll(tempList);
+        } else {
+            arrList.remove(str.trim());
+        }
+        arrList.sort(Comparator.naturalOrder());
+        System.out.println(arrList);
+        runProgram();
+    }
 }
--- ArrayListChallenge/src/dev/lpa/Main.java
+++ ArrayListChallenge/src/dev/lpa/Main.java
@@ -1,10 +1,69 @@
 package dev.lpa;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        Console main = new Console();
-        main.runProgram();
+//        Console main = new Console();
+//        main.runProgram();
+
+        boolean flag = true;
+        ArrayList<String> groceries = new ArrayList<>();
+        while (flag) {
+            printActions();
+            switch (Integer.parseInt(scanner.nextLine())) {
+                case 1 -> addItem(groceries);
+                case 2 -> removeItem(groceries);
+                default -> flag = false;
+            }
+            groceries.sort(Comparator.naturalOrder());
+            System.out.println(groceries);
+        }
+    }
+
+    private static void addItem(ArrayList<String> groceries) {
+
+        System.out.println("Add item(s) [separate items by comma]:");
+        String[] items = scanner.nextLine().split(",");
+//        groceries.addAll(List.of(items));
+
+        for (String item : items) {
+            String trimmed = item.trim();
+            if (groceries.indexOf(trimmed) < 0) {
+                groceries.add(trimmed);
+            }
+        }
+    }
+
+    private static void removeItem(ArrayList<String> groceries) {
+
+        System.out.println("Remove item(s) [separate items by comma]:");
+        String[] items = scanner.nextLine().split(",");
+
+        for (String item : items) {
+            String trimmed = item.trim();
+            groceries.remove(trimmed);
+        }
+    }
+
+    private static void printActions() {
+
+        String textBlock = """
+                    Available actions:
+                    
+                    0 - to shutdown
+                    
+                    1 - to add item(s) to list (comma delimited list)
+                    
+                    2 - to remove any items (comma delimited list)
+                    
+                    Enter a number for which action you want to do"):""";
+        System.out.printf(textBlock + " ");
     }
 }
