6e9d623926c31b0276c105ee3d4238dde8963230
Shawn <lcorel86@gmail.com>
2026-06-03 23:55:52 -0500

    list interator

--- LinkedList/src/dev/lpa/Main.java
+++ LinkedList/src/dev/lpa/Main.java
@@ -1,6 +1,7 @@
 package dev.lpa;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Main {
 
@@ -21,7 +22,7 @@ public class Main {
 
 //        gettingElements(placesToVisit);
 
-        printItinerary(placesToVisit);
+        printItinerary3(placesToVisit);
     }
 
     private static void addMoreElements(LinkedList<String> list) {
@@ -88,6 +89,30 @@ public class Main {
         System.out.println("Trip starts at " + list.getFirst());
         for (int i = 1; i < list.size(); i++) {
             System.out.println("--> From: " + list.get(i - 1) + " to " + list.get(i));
+        }
+        System.out.println("Tripe ends at " + list.getLast());
+    }
+
+    public static void printItinerary2(LinkedList<String> list) {
+
+        System.out.println("Trip starts at " + list.getFirst());
+        String previousTown = list.getFirst();
+        for (String town: list) {
+            System.out.println("--> From: " + previousTown + " to " + town);
+            previousTown = town;
+        }
+        System.out.println("Tripe ends at " + list.getLast());
+    }
+
+    public static void printItinerary3(LinkedList<String> list) {
+
+        System.out.println("Trip starts at " + list.getFirst());
+        String previousTown = list.getFirst();
+        ListIterator<String> iterator = list.listIterator(1);
+        while (iterator.hasNext()) {
+            var town = iterator.next();
+            System.out.println("--> From: " + previousTown + " to " + town);
+            previousTown = town;
         }
         System.out.println("Tripe ends at " + list.getLast());
     }
