e26ea34492d43eb06b8a6a8cc81ae50c79e98e2a
Shawn <lcorel86@gmail.com>
2026-06-10 20:29:22 -0500

    autoboxing challenge

---
+++ AutoboxingChallenge/src/dev/lpa/Bank.java
@@ -0,0 +1,11 @@
+package dev.lpa;
+
+import java.util.ArrayList;
+
+public class Bank {
+
+    private String name;
+    private ArrayList<Customer> customers;
+
+    public void addTransaction()
+}
---
+++ AutoboxingChallenge/src/dev/lpa/Customer.java
@@ -0,0 +1,17 @@
+package dev.lpa;
+
+import java.util.ArrayList;
+
+public class Customer {
+
+    private String name;
+    private ArrayList<Double> transactions;
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Double> getTransactions() {
+        return transactions;
+    }
+}
