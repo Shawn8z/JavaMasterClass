c8d97735511279a92be217e120377ba3eda9ae57
Shawn <lcorel86@gmail.com>
2026-06-18 22:17:08 -0500

    interface 1 done

--- Interfaces/src/dev/lpa/Animal.java
+++ Interfaces/src/dev/lpa/Animal.java
@@ -2,6 +2,9 @@ package dev.lpa;
 
 interface FlightEnabled {
 
+    public static final double MILES_TO_KM = 1.60934;
+    double KM_TO_MILES = 0.621371;
+
     void takeOff();
     void land();
     void fly();
